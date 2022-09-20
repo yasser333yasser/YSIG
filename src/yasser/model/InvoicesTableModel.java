bac9a34b299b31574e9b35bf47cd2e8cab779403
yasser333yasser <yasser333ama@gmail.com>
2022-09-20 07:52:38 +0200

    1st commit

---
+++ src/yasser/model/InvoicesTableModel.java
@@ -0,0 +1,45 @@
+
+package yasser.model;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+
+public class InvoicesTableModel extends AbstractTableModel {
+    private ArrayList<Invoice> invoices;
+    private String[] columns = {"No.", "Date", "Customer", "Total"};
+    public InvoicesTableModel(ArrayList<Invoice> invoices) {
+        this.invoices = invoices;
+    }
+    
+
+    @Override
+    public int getRowCount() {
+      return invoices.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+       return columns.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columns[column];
+    }
+    
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+         Invoice invoice = invoices.get(rowIndex);
+         
+         switch (columnIndex) {
+             case 0: return invoice.getNum();
+             case 1: return invoice.getDate();
+             case 2: return invoice.getCustomer();
+             case 3: return invoice.getInvoiceTotal();
+             default: return "";
+         }
+    }
+    
+}
---
+++ src/yasser/view/InvoiceDialog.java
@@ -0,0 +1,54 @@
+
+package yasser.view;
+
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
+public class InvoiceDialog extends JDialog {
+    private JTextField custNameField;
+    private JTextField invDateField;
+    private JLabel custNameLbl;
+    private JLabel invDateLbl;
+    private JButton okBtn;
+    private JButton cancelBtn;
+
+    public InvoiceDialog(InvoiceFrame frame) {
+        custNameLbl = new JLabel("Customer Name:");
+        custNameField = new JTextField(20);
+        invDateLbl = new JLabel("Invoice Date:");
+        invDateField = new JTextField(20);
+        okBtn = new JButton("OK");
+        cancelBtn = new JButton("Cancel");
+        
+        okBtn.setActionCommand("createInvoiceOK");
+        cancelBtn.setActionCommand("createInvoiceCancel");
+        
+        okBtn.addActionListener(frame.getController());
+        cancelBtn.addActionListener(frame.getController());
+        setLayout(new GridLayout(3, 2));
+        
+        add(invDateLbl);
+        add(invDateField);
+        add(custNameLbl);
+        add(custNameField);
+        add(okBtn);
+        add(cancelBtn);
+        
+        pack();
+        
+    }
+
+    public JTextField getCustNameField() {
+        return custNameField;
+    }
+
+    public JTextField getInvDateField() {
+        return invDateField;
+    }
+    
+}
